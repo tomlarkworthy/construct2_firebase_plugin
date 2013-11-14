3cbc2d22c50db8028a34f25e5e0bc159eb2f86d9
Tom Larkworthy <tom.larkworthy@gmail.com>
2013-11-14 12:18:27 +0000

    update build

--- .build.sh
+++ .build.sh
@@ -1,4 +1,30 @@
+#zip our build up
 zip firebase.zip firebase/*
-git add firebase.zip 
-git commit -m "autobuilt zip" firebase.zip 
-git push origin master
+
+
+#push to a page
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R firebase.zip  $HOME/coverage
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/tomlarkworthy/construct2_firebase_plugin.git  gh-pages > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/coverage/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with coverage\n"
+fi
