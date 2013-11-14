a521e1a7279be5202db0ce67b4584ffcf3212169
Tom Larkworthy <tom.larkworthy@gmail.com>
2013-11-14 12:36:48 +0000

    fix minor buig in travis build

--- .build.sh
+++ .build.sh
@@ -22,7 +22,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cp $HOME/firebase.zip .
 
   #add, commit and push files
-  git add -f $HOME/firebase.zip
+  git add -f firebase.zip
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
